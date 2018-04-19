929c40077664dca06b76f1dd41057492defc0d49
Fad198256 <s.elmo@tiscali.it>
2018-04-19 15:29:35 +0200

    abilito la selezione giorni festivi

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3064,7 +3064,10 @@ sap.ui.define([
 				var oYear2 = oYearN + 1;
 
 				// disabilito giorni festivi 
-				oCal2.addDisabledDate(new DateTypeRange({
+				// (SE) 19042018 commento perchè i giorni devono comunque essere selezionabili
+				// e la festività va inserita a mano se cade durante giorno feriale,
+				// inoltre qualcusno potrebbe lavorare anche se giorno festivo
+		/*		oCal2.addDisabledDate(new DateTypeRange({
 					startDate: this.oFormatYear.parse(oYearN + "0101")
 				}));
 
@@ -3152,7 +3155,7 @@ sap.ui.define([
 
 				oCal2.addDisabledDate(new DateTypeRange({
 					startDate: this.oFormatYear.parse(oYear2 + "1226")
-				}));
+				}));*/
 
 				var sRead = "/CalendarSet";
 
@@ -3299,7 +3302,10 @@ sap.ui.define([
 				var oYear2 = oYearN + 1;
 
 				// disabilito giorni festivi 
-				oCal1.addDisabledDate(new DateTypeRange({
+					// (SE) 19042018 commento perchè i giorni devono comunque essere selezionabili
+				// e la festività va inserita a mano se cade durante giorno feriale,
+				// inoltre qualcusno potrebbe lavorare anche se giorno festivo
+				/*oCal1.addDisabledDate(new DateTypeRange({
 					startDate: this.oFormatYear.parse(oYearN + "0101")
 				}));
 
@@ -3387,7 +3393,7 @@ sap.ui.define([
 
 				oCal1.addDisabledDate(new DateTypeRange({
 					startDate: this.oFormatYear.parse(oYear2 + "1226")
-				}));
+				}));*/
 
 				///////////////FINE FESTIVI////////////  
 				/*	var sOwnerId = this.getView()._sOwnerId;
